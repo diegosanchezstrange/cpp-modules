395ec29d8bf89b174f64391a0875c3a23bd34aed
diegosanchezstrange <diegosanchezstrangejavax@gmail.com>
2022-03-25 20:48:49 +0100

    chillando

--- cpp0/ex00/megaphone.cpp
+++ cpp0/ex00/megaphone.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
 
-int	main(void)
+int	main(int argc, char	**argv)
 {
-	std::cout << (Hello World!) << std::endl;
+	std::string str;
+	int	i, j;
+
+	i = 1;
+	j = 0;
+	if (argc < 2)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (0);
+	}
+	while (argc > i)
+	{
+		str = std::string(argv[i]);
+		while (str.length() > j)
+		{
+			std::cout << (char) std::toupper(str[j]);
+			j++;
+		}
+		j = 0;
+		i++;
+	}
+	std::cout << std::endl;
 	return (0);
 }
