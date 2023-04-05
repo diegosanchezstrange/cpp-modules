23680152d1ad1eebcc83e55ed4b4071966b4a5ca
diegosanchezstrange <diegosanchezstrangejavax@gmail.com>
2023-04-05 20:38:05 +0200

    errores

--- cpp09/ex01/RNP.cpp
+++ cpp09/ex01/RNP.cpp
@@ -8,6 +8,11 @@ void RNP::push(float num) { stack.push(num); }
 
 void RNP::operate(char operation)
 {
+    if (stack.size() < 2)
+    {
+        std::cout << "Not enough numbers in stack" << std::endl;
+        exit(1);
+    }
     float num1 = stack.top();
     stack.pop();
     float num2 = stack.top();
@@ -31,6 +36,16 @@ void RNP::operate(char operation)
 
 void RNP::print()
 {
+    if (stack.empty())
+    {
+        std::cout << "Stack is empty" << std::endl;
+        return;
+    }
+    if (stack.size() > 1)
+    {
+        std::cout << "Stack is not empty" << std::endl;
+        return;
+    }
     std::cout << stack.top() << std::endl;
     stack.pop();
 }
